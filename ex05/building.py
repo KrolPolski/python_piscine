c78aa06ff04186f3223c2aba8ba547b521436603
Ryan Boudwin <rboudwin@student.hive.fi>
2024-06-10 14:21:51 +0300

    ex05 complete

---
+++ ex05/building.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+import sys
+import string
+
+
+def count(str):
+    chars = len(str)
+    up = 0
+    lo = 0
+    pu = 0
+    di = 0
+    sp = 0
+    i = 0
+
+    while i < len(str):
+        if str[i] in string.digits:
+            di += 1
+        elif str[i] in string.ascii_uppercase:
+            up += 1
+        elif str[i] in string.ascii_lowercase:
+            lo += 1
+        elif str[i] in string.whitespace:
+            sp += 1
+        elif str[i] in string.punctuation:
+            pu += 1
+        i += 1
+    print(f"The text contains {chars} characters:")
+    print(f"{up} upper letters")
+    print(f"{lo} lower letters")
+    print(f"{pu} punctuation marks")
+    print(f"{sp} spaces")
+    print(f"{di} digits")
+
+
+def main():
+    main.__doc__ = """This program takes a single string argument
+    and displays the sums of its upper-case characters, lower-case
+    characters, punctuation characters, digits and spaces."""
+    assert len(sys.argv) < 3, "Too many arguments provided"
+    if len(sys.argv) != 2 or sys.argv[1] is None:
+        str = input("What is the text to count? ")
+    else:
+        str = sys.argv[1]
+    count(str)
+
+
+main()
