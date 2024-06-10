17cffb56eadd74d0636392381854efdd76798315
Ryan Boudwin <rboudwin@student.hive.fi>
2024-06-10 13:23:54 +0300

    ex04 completed

--- ex04/whatis.py
+++ ex04/whatis.py
@@ -12,10 +12,15 @@
 # **************************************************************************** #
 
 import sys
+sys.tracebacklimit = 0
+assert len(sys.argv) > 1, "No argument provided"
 assert len(sys.argv) < 3, "more than one argument is provided"
+is_integer = 1
 try:
     num = int(sys.argv[1])
 except ValueError:
+    is_integer = 0
+if is_integer == 0:    
     raise AssertionError("argument is not an integer")
 if num % 2 == 0:
     print("I'm Even")
