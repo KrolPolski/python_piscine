26b813086b20bc4a96362bbc278320f9637a15cd
Ryan Boudwin <rboudwin@student.hive.fi>
2024-06-11 10:47:37 +0300

    initial error detection completed for ex01

---
+++ Day-01/ex01/array2d.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+def slice_me(family: list, start: int, end: int) -> list:
+    assert type(family) is list, "family parameter must be a list"
+    assert type(start) is int, "Start must be an integer"
+    assert type(end) is int, "End must be an integer"
+    for a in family:
+        assert len(a) == len(family[0]), "Sublists must be the same length"
+    print(f"My shape is : ({len(family)}, {len(family[0])})")
+
+    
+def main():
+    slice_me([[2, 100], [1.65, 70], [1.75, 80]], 0, 2)
+if __name__ == "__main__":
+    main()
