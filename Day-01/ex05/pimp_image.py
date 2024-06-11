2018b4ea192776ee918f03f90ae601ca4d59c846
Ryan Boudwin <rboudwin@student.hive.fi>
2024-06-11 17:11:56 +0300

    blue filter working

--- Day-01/ex05/pimp_image.py
+++ Day-01/ex05/pimp_image.py
@@ -56,14 +56,30 @@ def ft_green(array):# -> array:
     print("greening complete")
     return inv_img
 #your code here
-#def ft_blue(array) -> array:
+def ft_blue(array):# -> array:
+    inv_img = (array)
+    print("Beginning blueing process")
+    a = 0
+    b = 0
+    c = 0
+    while a < len(inv_img):
+        while b < len(inv_img[0]):
+            while c < len(inv_img[0][0]) - 1:
+                inv_img[a][b][c] = 0
+                c += 1
+            c = 0
+            b += 1
+        b = 0
+        a += 1
+    print("Blueing complete")
+    return inv_img
 #your code here
 #def ft_grey(array) -> array:
 #your code here
 
 def main():
     img = ft_load('landscape.jpg')
-    mod_img = ft_green(img)
+    mod_img = ft_blue(img)
     plt.imshow(mod_img)
     plt.show()
 
