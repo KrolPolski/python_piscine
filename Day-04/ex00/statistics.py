dd976e944f4d7cbfd9090fa430512bd958781811
Ryan Boudwin <rboudwin@student.hive.fi>
2024-06-14 12:01:43 +0300

    median working

--- Day-04/ex00/statistics.py
+++ Day-04/ex00/statistics.py
@@ -7,6 +7,15 @@ def ft_mean(args: any):
         total += (args[i])
     return total / len(args) 
 
+def ft_median(args: any):
+    print(args)
+    sorted_args = sorted(args)
+    print(sorted_args)
+    if len(sorted_args) % 2 == 1:
+        return sorted_args[(len(sorted_args) // 2)]
+    else:
+        return ft_mean([sorted_args[(len(sorted_args)// 2)], sorted_args[(len(sorted_args)// 2) - 1]])
+    
 
 def ft_statistics(*args: any, **kwargs: any) -> None:
     if len(args) == 0 or len(kwargs) == 0:
@@ -15,13 +24,18 @@ def ft_statistics(*args: any, **kwargs: any) -> None:
     for kwarg in kwargs:
         if kwargs[kwarg] == 'mean':
             print(f"mean : {ft_mean(args)}")
+        elif kwargs[kwarg] == 'median':
+            print(f"median : {ft_median(args)}")
         print(f"kwarg: {kwarg}: {kwargs[kwarg]}")
 
 
 def main():
-    print("Arg print test")
+    print("mean test")
     ft_statistics(2, 4, 8, 16, first='mean')
-    print("Kwarg print test")
+    print("median test")
+    ft_statistics(5, 3, 1, 2, 4, first='median')
+    ft_statistics(3, 2, 1, 4, first='median')
+    ft_statistics(1, first='median')
     ft_statistics(first='plus', second='minus', third='multiply', fourth='divide')
     print("empty arg test")
     ft_statistics()
