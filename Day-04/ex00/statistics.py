bb292a0a0133b76ecbdb2860bad6c0aff7811b74
Ryan Boudwin <rboudwin@student.hive.fi>
2024-06-14 12:30:12 +0300

    quartile working for even sets

--- Day-04/ex00/statistics.py
+++ Day-04/ex00/statistics.py
@@ -8,14 +8,25 @@ def ft_mean(args: any):
     return total / len(args) 
 
 def ft_median(args: any):
-    print(args)
+   # print(args)
     sorted_args = sorted(args)
-    print(sorted_args)
+    #print(sorted_args)
     if len(sorted_args) % 2 == 1:
         return sorted_args[(len(sorted_args) // 2)]
     else:
         return ft_mean([sorted_args[(len(sorted_args)// 2)], sorted_args[(len(sorted_args)// 2) - 1]])
-    
+
+def ft_quartile(args: any):
+    #print(args)
+    sorted_args = sorted(args)
+   #print(sorted_args)
+    length = len(sorted_args)
+    median = ft_median(sorted_args)
+    lower_half = [arg for arg in sorted_args if arg <= median]
+    upper_half = [arg for arg in sorted_args if arg >= median]
+    print(f"lower_half is {lower_half}")
+    print(f"upper-half is {upper_half}")
+    return ([float(ft_median(lower_half)), float(ft_median(upper_half))])
 
 def ft_statistics(*args: any, **kwargs: any) -> None:
     if len(args) == 0 or len(kwargs) == 0:
@@ -26,6 +37,11 @@ def ft_statistics(*args: any, **kwargs: any) -> None:
             print(f"mean : {ft_mean(args)}")
         elif kwargs[kwarg] == 'median':
             print(f"median : {ft_median(args)}")
+        elif kwargs[kwarg] == 'quartile':
+            print(f"quartile : {ft_quartile(args)}")
+        else:
+            print("ERROR")
+            return
         print(f"kwarg: {kwarg}: {kwargs[kwarg]}")
 
 
@@ -36,6 +52,9 @@ def main():
     ft_statistics(5, 3, 1, 2, 4, first='median')
     ft_statistics(3, 2, 1, 4, first='median')
     ft_statistics(1, first='median')
+    print("quartile tests")
+    print("For list [5,4,3,2,1,6,7,8,10,9]")
+    ft_statistics(5,4,3,2,1,6,7,8,10,9, first='quartile')
     ft_statistics(first='plus', second='minus', third='multiply', fourth='divide')
     print("empty arg test")
     ft_statistics()
